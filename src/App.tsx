43e3c5b5a50397ec91490a553babd29f1552a56f
LarsFlieger <a.purenetwork@gmail.com>
2025-10-26 12:11:08 +0100

    chore: refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { GameState } from "./components/GameState";
+import { TwitchAuthGate } from "./components/ui/TwitchAuthGate";
+import { useAuthStore } from "./hooks/useAuthStore";
 import { Overlay } from "./pages/Overlay";
+import Panel from "./pages/Panel";
 
 const extType = import.meta.env.VITE_EXT_TYPE;
 
@@ -7,7 +11,18 @@ export const App: React.FC = () => {
     console.error("No valid extension type specified in .env");
   }
 
-  // const { token, channelId, isAuthenticated } = useAuthStore();
+  const { token, channelId, isAuthenticated } = useAuthStore();
 
-  return <Overlay />;
+  return (
+    <TwitchAuthGate>
+      {isAuthenticated && token && channelId ? (
+        <GameState token={token} channelId={channelId}>
+          {extType === "overlay" && <Overlay />}
+          {extType === "panel" && <Panel />}
+        </GameState>
+      ) : (
+        <div>Preparing your session...</div>
+      )}
+    </TwitchAuthGate>
+  );
 };
