dfb1967d5593589cbc5eceb2bc151452400e6151
LarsFlieger <a.purenetwork@gmail.com>
2025-10-26 13:49:33 +0100

    feat: add commented structure for inventory items

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,3 +1,17 @@
 export const Inventory: React.FC = () => {
   return <section className="pointer-events-auto">Inventory</section>;
 };
+
+// LEFT
+// helmet: { type: Helmet, property: 'helmet' },
+// chest: { type: Chest, property: 'chest' },
+// pants: { type: Pants, property: 'pants' },
+// boots: { type: Boots, property: 'boots' },
+//MAIN: mainHand: { type: HoldableItem, property: 'mainHand' },
+
+// RIGHT
+// amulet: { type: Amulet, property: 'amulet' }
+// gloves: { type: Glove, property: 'gloves' },
+// firstRing: { type: Ring, property: 'firstRing' },
+// secondRing: { type: Ring, property: 'secondRing' },
+//MAIN: offHand: { type: HoldableItem, property: 'offHand' },
