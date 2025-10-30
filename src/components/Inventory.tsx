6ed1d6682b99dc798de2d737336b4d993f805680
Lars Flieger <a.purenetwork@gmail.com>
2025-10-30 16:03:39 +0100

    chore: add implementation for equiptments

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -2,6 +2,22 @@ import { DndContext, DragCancelEvent, DragEndEvent, DragStartEvent, useDraggable
 import { CSS } from '@dnd-kit/utilities';
 import { useState } from "react";
 
+
+// LEFT
+// helmet: { type: Helmet, property: 'helmet' },
+// chest: { type: Chest, property: 'chest' },
+// pants: { type: Pants, property: 'pants' },
+// boots: { type: Boots, property: 'boots' },
+//MAIN: mainHand: { type: HoldableItem, property: 'mainHand' },
+
+// RIGHT
+// amulet: { type: Amulet, property: 'amulet' }
+// gloves: { type: Glove, property: 'gloves' },
+// firstRing: { type: Ring, property: 'firstRing' },
+// secondRing: { type: Ring, property: 'secondRing' },
+//MAIN: offHand: { type: HoldableItem, property: 'offHand' },
+
+
 export const Inventory: React.FC = () => {
 
   const [targetId, setTargetId] = useState<string>("")
