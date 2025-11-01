6f3b5c700a972e064d3de4f0501d9fe13293c0a1
Lars Flieger <a.purenetwork@gmail.com>
2025-11-01 23:05:17 +0100

    fix: bug with wrong size of item display

--- src/components/inventory/ItemDisplay.tsx
+++ src/components/inventory/ItemDisplay.tsx
@@ -3,5 +3,5 @@ import { Item } from "./types";
 
 export const ItemDisplay: React.FC<{ item: Item }> = ({ item }) => {
 
-    return <div className="text-xs bg-blue-100 cursor-grab">{item.name} <span className="text-red-500">{item.type}</span></div>;
+    return <div className="text-xs bg-blue-100 size-12 cursor-grab">{item.name} <span className="text-red-500">{item.type}</span></div>;
 }
