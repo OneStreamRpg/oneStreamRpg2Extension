f08a30d0b38c6e5eb3de118ddc1ad19f8f86513f
LarsFlieger <a.purenetwork@gmail.com>
2025-12-22 12:06:00 +0100

    feat: add window container

---
+++ src/components/ui/WindowContainer.tsx
@@ -0,0 +1,33 @@
+export const WindowContainer: React.FC<{
+  children: React.ReactNode;
+  className?: string;
+}> = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`${className}`}
+      style={{
+        color: "white",
+        boxShadow: [
+          "inset 0 2px 0 rgb(255, 255, 255, 0.1)", // top
+          "inset 6px 0 0 #343131", // right
+          "inset -6px 0 0 #343131", // left
+          "inset 0 4px 0 rgb(255, 255, 255, 0.1)", // top
+          "inset 0 6px 0 #343131", // top
+          "inset 0 -2px 0 #343131", // bottom
+          "inset 0 -4px 0 rgb(255, 255, 255, 0.1)", // bottom
+          "inset 0 -6px 0 #343131", // bottom
+          "inset 0px 0px 20px -5px black",
+          "0px 0px 8px 0px black",
+        ].join(", "),
+        borderTop: "3px solid #3f4347",
+        borderBottom: "3px solid #28201b",
+        borderLeft: "3px solid #28201b",
+        borderRight: "3px solid #28201b",
+        backgroundColor: "#111011",
+        padding: "8px",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
