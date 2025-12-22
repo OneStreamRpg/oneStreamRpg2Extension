e3b2ac4382652e71e334251f51d3458be3bbb5a6
LarsFlieger <a.purenetwork@gmail.com>
2025-12-22 12:06:09 +0100

    feat: add window container to inv

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,5 +1,6 @@
 import { useUIStore } from "../store/useUIStore";
 import { PagePosition } from "../types/ui";
+import { WindowContainer } from "./ui/WindowContainer";
 
 export const PageLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -8,8 +9,8 @@ export const PageLayout: React.FC<{ children: React.ReactNode }> = ({
   const pagePosition = useUIStore((state) => state.pagePosition);
 
   return (
-    <section
-      className={`h-full bg-red-500/20 w-86 overflow-y-scroll overflow-x-hidden p-4 ${
+    <WindowContainer
+      className={`h-full w-86 overflow-y-scroll overflow-x-hidden p-4 ${
         pagePosition === PagePosition.RIGHT && "ml-auto"
       }`}
     >
@@ -24,6 +25,6 @@ export const PageLayout: React.FC<{ children: React.ReactNode }> = ({
         </nav>
         {children}
       </div>
-    </section>
+    </WindowContainer>
   );
 };
