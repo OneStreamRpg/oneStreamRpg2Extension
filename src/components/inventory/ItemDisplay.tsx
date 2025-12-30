81e670d9f114b0dda93ff69241dfa8f22d009654
LarsFlieger <a.purenetwork@gmail.com>
2025-12-30 13:17:33 +0100

    chore: add test code to itemdisplay

--- src/components/inventory/ItemDisplay.tsx
+++ src/components/inventory/ItemDisplay.tsx
@@ -1,25 +1,38 @@
+import { metadataService } from "../../services/MetadataService";
 import { InventoryItem } from "../../types/personalChannel";
 
+const DISPLAY_ICON = false;
+
 export const ItemDisplay: React.FC<{ item: InventoryItem }> = ({ item }) => {
+  const itemData = metadataService.getItemSync(item.itemId);
+
   return (
     <div
       className="text-xs bg-gray-800 text-white size-16 cursor-grab relative"
       data-item-id={item.id}
+      title={itemData?.name ?? item.itemId}
     >
-      <p className="absolute">
-        {item.itemId}{" "}
-        <span className="text-red-500">
-          {item.id}: {item.quantity}
+      {DISPLAY_ICON ? (
+        <img
+          src={`https://cdn.onestreamrpg.com/images/items/${itemData.icon}.png`}
+          alt={itemData.name ?? item.itemId}
+          className="size-16 mx-auto"
+          style={{
+            imageRendering: "pixelated",
+          }}
+        />
+      ) : (
+        <>
+          <p>{itemData.name}</p>
+          <p className="">{itemData.type}</p>
+          <p className="">{itemData.rarity}</p>
+        </>
+      )}
+      {item.quantity > 1 && (
+        <span className="absolute bottom-0 right-0.5 text-white text-shadow font-bold">
+          {item.quantity}
         </span>
-      </p>
-      {/* <img
-        src={`https://cdn.onestreamrpg.com/images/items/${item.icon}.png`}
-        alt={item.itemId}
-        className="size-16 image-re mx-auto"
-        style={{
-          imageRendering: "pixelated",
-        }}
-      /> */}
+      )}
     </div>
   );
 };
