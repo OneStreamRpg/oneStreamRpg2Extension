8d2899bec404291742c2141c4a36d30775512791
LarsFlieger <a.purenetwork@gmail.com>
2026-01-08 17:20:37 +0100

    fix: base gameObjects

--- src/services/gameObjects.ts
+++ src/services/gameObjects.ts
@@ -2,32 +2,22 @@ import { Enemy, GameObject, GameState, Npc, Player } from "../types/gameState";
 
 export function mapPlayers(players: any[]): Player[] {
   return players.map((p): Player => ({
-    id: p.playerId,
-    name: p.username,
-    twitchId: p.twitchId,
-    username: p.username,
+    ...p,
     type: "player",
-    hitbox: p.hitbox,
   }));
 }
 
 export function mapEnemies(enemies: any[]): Enemy[] {
   return enemies.map((e): Enemy => ({
-    id: e.enemyId,
-    name: e.name,
-    level: e.level,
-    hp: e.hp,
+    ...e,
     type: "enemy",
-    hitbox: e.hitbox,
   }));
 }
 
 export function mapNpcs(npcs: any[]): Npc[] {
   return npcs.map((npc): Npc => ({
-    id: npc.npcId,
-    name: npc.name,
+    ...npc,
     type: "npc",
-    hitbox: npc.hitbox,
   }));
 }
 
