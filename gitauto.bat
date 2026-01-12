1d7349c3a7c0efa549532eb0a302ed8cc71dbf9c
LarsFlieger <a.purenetwork@gmail.com>
2026-01-12 14:04:47 +0100

    refactor: cleanup .env

--- gitauto.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /D "%~dp0"
-git add .
-set /p id="Enter Message:"
-git commit -m "%id%_"
-git push
-exit
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
     readonly VITE_EXT_TYPE: "panel" | "overlay"
     readonly VITE_SOCKET_URL: string
     readonly VITE_DEVELOPER: "true" | "false"
+    readonly VITE_DISABLE_BACKEND: "true" | "false"
 }
 
 interface ImportMeta {
