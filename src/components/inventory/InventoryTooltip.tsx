1eecc4c3c8b3b0f3e00391c1d1351cf7c43cbaf9
Lars Flieger <a.purenetwork@gmail.com>
2026-02-19 11:39:06 +0000

    feat: update InventoryTooltip layout and enhance item display details

--- src/components/inventory/InventoryTooltip.tsx
+++ src/components/inventory/InventoryTooltip.tsx
@@ -28,35 +28,36 @@ export const InventoryTooltip: React.FC<{ item: Item }> = ({ item }) => {
   // }
 
   return (
-    <div className="flex bg-amber-700">
-      <div className="bg-gray-900">
-        <header className="flex">
-          <img
-            src={`https://cdn.onestreamrpg.com/images/items/batSword.png`}
-            alt={"Item Icon"}
-            className="size-24 m-4"
-            style={{
-              imageRendering: "pixelated",
-            }}
-          />
-          <section>
-            <h2 className="text-lg font-bold">{itemData.name ?? `Unknown Item Name (${item.itemId})`}</h2>
-            <p>{itemData.rarity && `${itemData.rarity}`}{itemData.type && `${itemData.rarity ? " " : ""}${itemData.type}`}</p>
-            <p>Damage</p>
-            <p>Attack speed</p>
-            <p>Damage Scaling</p>
-            <p>Strength</p>
-            <p>Health</p>
-          </section>
-        </header>
-        <main>
-          <p>{itemData.description}</p>
-        </main>
-        <footer className="flex">
-          <p>Requirements comming soon...</p>
-          {itemData.value && <p className="pl-2 ml-auto text-right">Sell Price: {itemData.value ?? 0}g</p>}
-        </footer>
-      </div>
+    <div className="w-80">
+      <header className="flex border-b-2 border-gray-500">
+        <img
+          src={`https://cdn.onestreamrpg.com/images/items/batSword.png`}
+          alt={"PLACEHOLDER"}
+          className="size-24 m-4"
+          style={{
+            imageRendering: "pixelated",
+          }}
+        />
+        <section>
+          <h2 className="text-lg font-bold" title={itemData.itemId ?? item.itemId}>{itemData.name ?? `Unknown Item Name (${item.itemId})`}</h2>
+          <p>{itemData.rarity && `${itemData.rarity}`}{itemData.type && `${itemData.rarity ? " " : ""}${itemData.type}`}</p>
+          {itemData.damageType && <p>{itemData.damageType}</p>}
+          {itemData.attackSpeed && <p>{itemData.attackSpeed}</p>}
+          <p className="italic">Damage Scaling</p>
+          <p className="italic">Strength</p>
+          <p className="italic">Health</p>
+        </section>
+      </header>
+      <main className="border-b-2 border-gray-500">
+        {itemData.stats && <div className="border-b-2 border-gray-500">{Object.entries<"string">(itemData.stats).map(([statKey, statValue]) => (
+          <p key={statKey}>{statKey.charAt(0).toUpperCase() + statKey.slice(1)}: {statValue}</p>
+        ))}</div>}
+        <p>{itemData.description}</p>
+      </main>
+      <footer className="flex">
+        <p className="text-sm text-gray-500">Requirements</p>
+        {itemData.value && <p className="pl-2 ml-auto text-right">Sell Price: {itemData.value ?? 0}g</p>}
+      </footer>
     </div>
   );
 };
